af298309b1143d63b7e89de3d2fdd6601803a8b8
alexander <da070116@gmail.com>
2020-08-13 19:23:40 +0300

    initial

---
+++ enterest/tokenusers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TokenusersConfig(AppConfig):
+    name = 'tokenusers'
---
+++ enterest/tokenusers/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# https://pythondigest.ru/view/34325/
+# https://djangochannel.com/blog/django/django_rest_framework_autentifikatsii_polzovatelej-337/
